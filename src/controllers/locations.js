bed3e489916e99f4f274925ad16bc3f0b1e9384c
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-12-07 08:03:45 +0200

    added locations backEnd

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -2,26 +2,47 @@ import mongoose from "mongoose";
 import LocationModel from "../models/locations.js";
 
 const ADD_LOCATION = async (req, res) => {
-  const location = new LocationModel({
-    title: req.body.title,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    location_photo_url: req.body.location_photo_url,
-    description: req.body.description,
-    location_id: req.body.location_id,
-  });
-  const response = await location.save();
+  try {
+    const location = new LocationModel({
+      title: req.body.title,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      location_photo_url: req.body.location_photo_url,
+      description: req.body.description,
+      location_id: req.body.location_id,
+    });
+    const response = await location.save();
 
-  return res.status(200).json({ response });
+    return res.status(200).json({ response });
+  } catch (err) {
+    console.error("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
 };
 
 const GET_LOCATIONS = async (req, res) => {
-  const locations = await LocationModel.find();
-  return res.status(200).json({ locations });
+  try {
+    const locations = await LocationModel.find();
+    return res.status(200).json({ locations });
+  } catch (err) {
+    console.error("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
 };
+
 const GET_LOCATION_BY_ID = async (req, res) => {
-  const locations = await LocationModel.findOne({ _id: req.params.id });
-  return res.status(200).json({ locations });
+  try {
+    const location = await LocationModel.findOne({ _id: req.params.id });
+
+    if (location) {
+      return res.status(200).json({ location });
+    } else {
+      return res.status(404).json({ response: "Location not found" });
+    }
+  } catch (err) {
+    console.error("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
 };
 
 const UPDATE_LOCATION = async (req, res) => {
@@ -38,8 +59,21 @@ const UPDATE_LOCATION = async (req, res) => {
 };
 
 const DELETE_LOCATION = async (req, res) => {
-  const locations = await LocationModel.deleteOne({ _id: req.params.id });
-  return res.status(200).json({ locations });
+  try {
+    const locations = await LocationModel.deleteOne({ _id: req.params.id });
+
+    // Check if the deletion was successful
+    if (locations.deletedCount === 1) {
+      return res
+        .status(200)
+        .json({ response: "Location deleted successfully" });
+    } else {
+      return res.status(404).json({ response: "Location not found" });
+    }
+  } catch (err) {
+    console.error("ERROR: ", err);
+    res.status(500).json({ response: "Something went wrong!" });
+  }
 };
 
 export {
