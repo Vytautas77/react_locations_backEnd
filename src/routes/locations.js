2471523be27ddbd04087a925fc344ab147fc6aa3
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-12-12 17:13:37 +0200

    added template

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/locations", auth, ADD_LOCATION);
 router.get("/locations", auth, GET_LOCATIONS);
-router.get("/locations/:id", GET_LOCATION_BY_ID);
+router.get("/locations/:id", auth, GET_LOCATION_BY_ID);
 router.put("/locations/:id", auth, UPDATE_LOCATION);
 router.delete("/locations/:id", auth, DELETE_LOCATION);
 
